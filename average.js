864b2b9a7069a1ddeee2529193a030190c6184bc
Punit Maheshwari <punitmahes@gmail.com>
2021-12-08 16:50:10 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,8 +1,8 @@
 
 function average(numbers) {
-  if(numbers.length == 0) return 0;
-  const sum = numbers.reduce((p, c)=> p + c, 0);
-  return sum / numbers.length;
+  if(numbers.length == 0) return NaN;
+  const sum = numbers.reduce((p, {c})=> p + c, 0);
+  return (sum / numbers.length) || 0;
 }
 
 module.exports = {average};
